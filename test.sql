5e7bee28e566a667b606620b239526aac651ca1b
xyh <thisxyh@gmail.com>
2019-10-10 18:06:37 +0800

    单元测试数据库脚本。

--- test.sql
+++ test.sql
@@ -1,2 +1,37 @@
-﻿-- 单元测试使用的数据库脚本
+﻿-- Mssql 单元测试使用的数据库脚本
 
+-- Create a new database called 'DapperClient_Db'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'DapperClient_Db'
+)
+CREATE DATABASE DapperClient_Db
+GO
+
+
+use DapperClient_Db
+GO
+
+-- Create a new table called 'Person' in schema
+-- Drop the table if it already exists
+IF OBJECT_ID('Person', 'U') IS NOT NULL
+DROP TABLE Person
+GO
+-- Create the table in the specified schema
+CREATE TABLE Person
+(
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1, 1), -- primary key column
+    [Name] [NVARCHAR](8) NOT NULL,
+    Age INT NOT NULL,
+    -- specify more columns here
+    Birthday DATETIME NULL,
+    height FLOAT NULL,
+    [weight] FLOAT NULL,
+    InsertTime DATETIME NOT NULL DEFAULT getdate()
+);
+GO
